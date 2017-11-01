5e977ef770ec214c7c5409f755655fc274c85fd2
AmandaSanchez21 <32260263+AmandaSanchez21@users.noreply.github.com>
2017-11-01 14:15:32 +0100

    Add files via upload

--- src/Domain/Problem.java
+++ src/Domain/Problem.java
@@ -1,5 +1,4 @@
 package Domain;
-
 import java.io.*;
 import java.util.*;
 
@@ -41,7 +40,58 @@ public class Problem {
 		actions.get(i).getSand_n() + " S:" + actions.get(i).getSand_s() + " E:" + actions.get(i).getSand_e() + " W:" + actions.get(i).getSand_w() + "]");
 		}
 		
-		List<Node> frontier = t.createFrontier();
+		
+		PriorityQueue<Node> frontier = new PriorityQueue<Node>();
+		
+		Node initialState = new Node();
+		initialState.setState(t);
+		
+		frontier.offer(initialState);
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}	
 	
+	
+	public static boolean isGoal (State st) {
+		int [][] field = st.getField();
+		
+		for (int i=0; i<field.length; i++) {
+			for (int j=0; j<field.length; j++) {
+				if(field[i][j] != st.getK()) {
+					return false;
+				}
+			}
+		}	
+		return true;
+	}
+	
+	public static State applyAction (State st, Action ac) {
+		State newState = new State();
+		int [][] newField = st.getField();
+		int pos = newField[st.getX()][st.getY()];
+		int currentSand = ac.getSand_e() + ac.getSand_n() + ac.getSand_s() + ac.getSand_w();
+		int newPos = pos - currentSand;
+		newField[st.getX()][st.getY()] = newPos;
+		
+		if(ac.getSand_n() > 0 && st.getX()-1 > 0 && newField[st.getX()-1][st.getY()] + ac.getSand_n() <= st.getMax()) {
+			newField[st.getX()-1][st.getY()] += ac.getSand_n();
+		} else if(ac.getSand_s() > 0 && st.getX()+1 < newField.length && newField[st.getX()+1][st.getY()] + ac.getSand_s() <= st.getMax()) {
+			newField[st.getX()+1][st.getY()] += ac.getSand_s();
+		} else if(ac.getSand_w() > 0 && st.getY()-1 > 0 && newField[st.getX()][st.getY()-1] + ac.getSand_w() <= st.getMax()) {
+			newField[st.getX()][st.getY()-1] += ac.getSand_w();
+		} else if(ac.getSand_e() > 0 && st.getY()+1 < newField.length && newField[st.getX()][st.getY()+1] + ac.getSand_e() <= st.getMax()) {
+			newField[st.getX()][st.getY()+1] += ac.getSand_e();
+		}
+		
+		newState.setField(newField);
+		return newState;
+	}
+
 }
