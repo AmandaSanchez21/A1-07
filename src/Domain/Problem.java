96ab38fbbf70fbcddd1ca7350b91cf4bec8dc1d0
AmandaSanchez21 <32260263+AmandaSanchez21@users.noreply.github.com>
2017-12-06 19:30:28 +0100

    Add files via upload

--- src/Domain/Problem.java
+++ src/Domain/Problem.java
@@ -1,4 +1,5 @@
 package Domain;
+
 import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -8,287 +9,274 @@ import persistance.FileHandler;
 import persistance.InputExceptions;
 
 public class Problem {
-	
-	private static final char[] CONSTS_HEX = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
 
-	public static void main (String[]args) throws IOException, InputExceptions {
-		State t = new State(); 
+	private static int spatial_complexity;
+	private static long time_complexity;
+	private static Hashtable<String,Integer> visited = new Hashtable<String, Integer>();;
+	private static final char[] CONSTS_HEX = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+	public static void main(String[] args) throws IOException, InputExceptions {
+		State t = new State();
 		FileHandler.readFile(t);
-		State newState = t; 
-		int [][]field = t.getField();
+		int[][] field = t.getField();
 		boolean optimization;
-		
-		
+
 		System.out.println();
-		
-		for (int i1=0; i1<field.length; i1++) {
-			for(int j=0; j<field.length; j++) {
+
+		for (int i1 = 0; i1 < field.length; i1++) {
+			for (int j = 0; j < field.length; j++) {
 				System.out.print(field[i1][j]);
 			}
 			System.out.println();
 		}
-		List<Action>actions = State.successor(t);
-		List<Movement> movements = State.moveTractor(t);
-		System.out.println();
-		System.out.println("Possible movements: ");
-		
-		for (int i=0; i<movements.size(); i++) {
-			movements.get(i).printMove();
-		}
-		
-		System.out.println();
-		System.out.println();
-		
-		System.out.println("Possible actions: ");
-		
-		/*for(int i=0; i<actions.size(); i++) {
-			System.out.println("[(" + actions.get(i).getNext_move().getX() + "," + actions.get(i).getNext_move().getY() + ")"  + " N:" +
-		actions.get(i).getSand_n() + " S:" + actions.get(i).getSand_s() + " E:" + actions.get(i).getSand_e() + " W:" + actions.get(i).getSand_w() + "]");
-		}*/
-		
-		
+
 		String strategy;
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please, enter the strategy\nBFS, DFS, DLS, ILS, UCS or A*");
+		System.out.println("Please, enter the strategy\nBFS, DFS, DLS, IDS, UCS or A*");
 		strategy = sc.nextLine();
 		strategy = strategy.toUpperCase();
-		
-		while (!strategy.equals("BFS") && !strategy.equals("DFS") && !strategy.equals("DLS") && !strategy.equals("ILS") && !strategy.equals("UCS") && !strategy.equals("A*")) {
-			System.out.println("Please, enter a correct strategy \nBFS, DFS, DLS, ILS, UCS or A*");
+
+		while (!strategy.equals("BFS") && !strategy.equals("DFS") && !strategy.equals("DLS") && !strategy.equals("IDS")
+				&& !strategy.equals("UCS") && !strategy.equals("A*")) {
+			System.out.println("Please, enter a correct strategy \nBFS, DFS, DLS, IDS, UCS or A*");
 			strategy = sc.nextLine().toUpperCase();
 		}
-		
+
 		System.out.println("Now, enter the depth desired.");
 		int depth = sc.nextInt();
-		
-		while(depth<=0) {
+
+		while (depth <= 0) {
 			System.out.println("Please, enter a valid depth");
 			depth = sc.nextInt();
 		}
-		
+
 		System.out.println("Do you want to use optimization? (Y/N)");
 		String opt = sc.next().toUpperCase();
-		
-		while(!opt.equals("Y") && !opt.equals("N")) {
+
+		while (!opt.equals("Y") && !opt.equals("N")) {
 			System.out.println("Please, enter a valid option (Y/N)");
 			opt = sc.next().toUpperCase();
 		}
-		
+
 		if (opt.equals("Y")) {
 			optimization = true;
 		} else {
 			optimization = false;
 		}
-		
+
 		long time = System.currentTimeMillis();
 		List<Node> solution = boundedSearch(t, strategy, depth, optimization);
 		long time1 = System.currentTimeMillis();
-		long total = time1 - time;
-		System.out.println(total);
-		printSolution(solution);
-	
+		time_complexity = time1 - time;
+		
+		if(solution != null) {
+			printSolution(solution);
+			
+			System.out.println();
+			System.out.println("Do you wanna to keep the solution into a file? (Y/N)");
+			String f = sc.next().toUpperCase();
+			
+			
+			while (!f.equals("Y") && !f.equals("N")) {
+				System.out.println("Please, enter a valid option (Y/N)");
+				f = sc.next().toUpperCase();
+			}
+			
+			if(f.equals("Y")) {
+				FileHandler.writeFile(solution, time_complexity, spatial_complexity, strategy, optimization);
+			} 
+		}
+
 	}
-	
-	
+
 	public static List<Node> boundedSearch(State st, String strategy, int max_depth, boolean opt) throws IOException {
 		PriorityQueue<Node> frontier = new PriorityQueue<Node>();
-		Node initial_node = new Node(st, 0, 0, null); //Initial node with the initial state, cost and depth are 0 and there's no reference to a parent
+		Node initial_node = new Node(st, 0, 0, null); // Initial node with the initial state, cost and depth are 0 and
+														// there's no reference to a parent
 		initial_node.selectValueNode(strategy);
 		frontier.offer(initial_node);
+		spatial_complexity = 1;
 		Node current_node = new Node();
-		
-		Hashtable<String, Integer> visited = new Hashtable<String, Integer>();
-		
-		if (strategy.equals("BFS") || strategy.equals("DFS") || strategy.equals("DLS") || strategy.equals("ILS")) {
-			visited.put(encriptaEnMD5(initial_node.getState()), initial_node.getCost());
+
+		//Hashtable<String, Integer> visited = new Hashtable<String, Integer>();
+
+		if (strategy.equals("BFS") || strategy.equals("DFS") || strategy.equals("DLS") || strategy.equals("IDS")) {
+			visited.put(encryptState(initial_node.getState()), initial_node.getCost());
 		} else {
-			visited.put(encriptaEnMD5(initial_node.getState()), initial_node.getValue());
+			visited.put(encryptState(initial_node.getState()), initial_node.getValue());
 		}
-		
+
 		boolean solution = false;
 
-		
-		while(!frontier.isEmpty() && solution==false) {
-			current_node = frontier.poll(); //Removes the node which is in the head of the queue
-			
-			
-			if(isGoal(current_node.getState())) {
+		while (!frontier.isEmpty() && solution == false) {
+			current_node = frontier.poll(); // Removes the node which is in the head of the queue
+
+			if (isGoal(current_node.getState())) {
 				solution = true;
 			} else {
-				List<Action> actions = State.successor(current_node.getState()); //Generates all the possible actions
-				List<Node> nodes = createNodeList(actions, current_node, max_depth, strategy, current_node.getState()); //Creates a node list where each node has a new state, 
+				List<Action> actions = State.successor(current_node.getState()); // Generates all the possible actions
+				List<Node> nodes = createNodeList(actions, current_node, max_depth, strategy, current_node.getState()); // Creates a node list where each
+																														// node has a new state, 
 																														//generated after applying the corresponding action
-				
-				if(opt) {
-					checkVisited(nodes,visited, strategy);
+
+				if (opt) {
+					checkVisited(nodes, visited, strategy);
 				}
-				
-				
-				for(int i=0; i<nodes.size(); i++) {
+
+				for (int i = 0; i < nodes.size(); i++) {
+					spatial_complexity += 1;
 					frontier.offer(nodes.get(i));
-				}	 
+				}
 			}
 		}
-		
-		/*for(int i=0; i<frontier.size(); i++) {
-			System.out.print(frontier.remove().getValue());
-		}
-		System.out.println();*/
-		
-		if(solution) {
+
+		if (solution) {
 			return createSolution(current_node);
 		} else {
 			System.out.println("No solution");
-			return null;	
+			return null;
 		}
 	}
-	
-	
-	
-	/*public static void checkVisited(List<Node> nodes, List<State> visited) {
-		for (int i=0; i<nodes.size(); i++) {
-			State st = nodes.get(i).getState();
-			int [][] field = st.getField();
-			for (int j=0; j<visited.size(); j++) {
-				int [][] field1 = visited.get(j).getField();
-				for(int x=0; x<field.length; x++) {
-					for(int z=0; z<field.length; z++) {
-						if(field[x][z]==field1[x][z]) {
-							if(st.getValue() < visited.get(j).getValue()) {
-								visited.get(j).setValue(st.getValue());
-								nodes.remove(i);
-							}
-						}
+
+	public static void checkVisited(List<Node> nodes, Hashtable<String, Integer> visited, String strategy) {
+		for (int i = 0; i < nodes.size(); i++) {
+			if (visited.containsKey(encryptState(nodes.get(i).getState()))) {
+				if (strategy.equals("BFS") || strategy.equals("DFS") || strategy.equals("DLS") || strategy.equals("IDS")) {
+					if (nodes.get(i).getCost() < visited.get(encryptState(nodes.get(i).getState()))) {
+						visited.replace((encryptState(nodes.get(i).getState())), nodes.get(i).getCost());
+					} else {
+						nodes.remove(i);
+					}
+				} else {
+					if (nodes.get(i).getValue() < visited.get(encryptState(nodes.get(i).getState()))) {
+						visited.replace((encryptState(nodes.get(i).getState())), nodes.get(i).getValue());
+					} else {
+						nodes.remove(i);
 					}
 				}
-			}
-		}
-	}*/
-	
-	
-	public static void checkVisited (List<Node> nodes, Hashtable<String, Integer> visited, String strategy) {
-		for (int i=0; i<nodes.size(); i++) {
-			if (visited.containsKey(encriptaEnMD5(nodes.get(i).getState()))) {
-				if(nodes.get(i).getValue() < visited.get(encriptaEnMD5(nodes.get(i).getState()))) {
-					visited.replace((encriptaEnMD5(nodes.get(i).getState())), nodes.get(i).getValue());
-					nodes.remove(i);
-				}
 			} else {
-				if (strategy.equals("BFS") || strategy.equals("DFS") || strategy.equals("DLS") || strategy.equals("ILS")) {
-					visited.put(encriptaEnMD5(nodes.get(i).getState()), nodes.get(i).getCost());
+				if (strategy.equals("BFS") || strategy.equals("DFS") || strategy.equals("DLS") || strategy.equals("IDS")) {
+					visited.put(encryptState(nodes.get(i).getState()), nodes.get(i).getCost());
 				} else {
-					visited.put(encriptaEnMD5(nodes.get(i).getState()), nodes.get(i).getValue());
+					visited.put(encryptState(nodes.get(i).getState()), nodes.get(i).getValue());
 				}
 			}
 		}
 	}
 
+	public static boolean isGoal(State st) {
+		int[][] field = st.getField();
 
-	public static boolean isGoal (State st) {
-		int [][] field = st.getField();
-		
-		for (int i=0; i<field.length; i++) {
-			for (int j=0; j<field.length; j++) {
-				if(field[i][j] != st.getK()) {
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field.length; j++) {
+				if (field[i][j] != st.getK()) {
 					return false;
 				}
 			}
-		}	
+		}
 		return true;
 	}
-	
-	public static State applyAction (State st, Action ac) {
-		
-		int [][] newField = st.getField();
-		int pos = newField[st.getX()][st.getY()]; //Amount of sand in the current position 
-		int movedSand = ac.getSand_e() + ac.getSand_n() + ac.getSand_s() + ac.getSand_w(); //Totally amount of sand to move
+
+	public static State applyAction(State st, Action ac) {
+
+		int[][] newField = st.getField();
+		int pos = newField[st.getX()][st.getY()]; // Amount of sand in the current position
+		int movedSand = ac.getSand_e() + ac.getSand_n() + ac.getSand_s() + ac.getSand_w(); // Totally amount of sand to
+																							// move
 		int newPos = pos - movedSand;
-		newField[st.getX()][st.getY()] = newPos; //Remaining sand in the current position 
-		/* If there is sand to move to the North, East, West or South && if it is possible to move to the North, East, West or South*/
-		if(ac.getSand_n() > 0 && st.getX()+1 >= 0) {
-			newField[st.getX()-1][st.getY()] += ac.getSand_n(); //Update the amount of sand of the field
-		} 
-		if(ac.getSand_s() > 0 && st.getX()+1 < newField.length) {
-			newField[st.getX()+1][st.getY()] += ac.getSand_s();
-		} 
-		if(ac.getSand_w() > 0 && st.getY()-1 >= 0) {
-			newField[st.getX()][st.getY()-1] += ac.getSand_w();
-		} 
-		if(ac.getSand_e() > 0 && st.getY()+1 < newField.length) {
-			newField[st.getX()][st.getY()+1] += ac.getSand_e();
+		newField[st.getX()][st.getY()] = newPos; // Remaining sand in the current position
+		/*
+		 * If there is sand to move to the North, East, West or South && if it is
+		 * possible to move to the North, East, West or South
+		 */
+		if (ac.getSand_n() > 0 && st.getX() + 1 >= 0) {
+			newField[st.getX() - 1][st.getY()] += ac.getSand_n(); // Update the amount of sand of the field
 		}
-	
+		if (ac.getSand_s() > 0 && st.getX() + 1 < newField.length) {
+			newField[st.getX() + 1][st.getY()] += ac.getSand_s();
+		}
+		if (ac.getSand_w() > 0 && st.getY() - 1 >= 0) {
+			newField[st.getX()][st.getY() - 1] += ac.getSand_w();
+		}
+		if (ac.getSand_e() > 0 && st.getY() + 1 < newField.length) {
+			newField[st.getX()][st.getY() + 1] += ac.getSand_e();
+		}
+
 		State newState = State.copyState(st, ac, newField);
 
 		return newState;
 	}
-	
-	public static List<Node> createNodeList (List<Action> actions, Node cn, int depth, String strategy, State st) {
-		List <Node> nodes = new ArrayList<Node>();
-		int new_depth = cn.getDepth() +1;
-		if(cn.getDepth() + 1 <= depth) {
-			for(int i=0; i<actions.size(); i++) {
+
+	public static List<Node> createNodeList(List<Action> actions, Node cn, int depth, String strategy, State st) {
+		List<Node> nodes = new ArrayList<Node>();
+		int new_depth = cn.getDepth() + 1;
+		if (cn.getDepth() + 1 <= depth) {
+			for (int i = 0; i < actions.size(); i++) {
 				State c_state = applyAction(st, actions.get(i));
-				Node aux = new Node(c_state, cn.getCost()+State.cost(actions.get(i)), new_depth, cn);
+				Node aux = new Node(c_state, cn.getCost() + State.cost(actions.get(i)), new_depth, cn);
 				aux.selectValueNode(strategy);
 				aux.getState().setValue(aux.getValue());
 				aux.setAction(actions.get(i));
 				nodes.add(aux);
 			}
 		}
-		
+
 		return nodes;
 	}
-	
-	
+
 	public static List<Node> createSolution(Node current_node) {
-		List <Node> solution = new ArrayList<Node>();
-		
+		List<Node> solution = new ArrayList<Node>();
+
 		while (current_node != null) {
 			solution.add(current_node);
-			current_node=current_node.getFather();
+			current_node = current_node.getFather();
 		}
-		
+
 		return solution;
 	}
-	
-	/*public static void checkVisited (List<Node> nodes, Hashtable<State, Integer> visited) {
-		for (int i=0; i<nodes.size(); i++) {
-			State st = nodes.get(i).getState();
-			
-			if(visited.containsKey(st)) {
-				if(nodes.get(i).getValue() < visited.get()
-			}
-		}
-	}*/
-	
-	public static void printSolution (List<Node>solution) {
-		int cost=0;
-		for (int i=solution.size()-1; i>=0; i--) {
+
+	/*
+	 * public static void checkVisited (List<Node> nodes, Hashtable<State, Integer>
+	 * visited) { for (int i=0; i<nodes.size(); i++) { State st =
+	 * nodes.get(i).getState();
+	 * 
+	 * if(visited.containsKey(st)) { if(nodes.get(i).getValue() < visited.get() } }
+	 * }
+	 */
+
+	public static void printSolution(List<Node> solution) {
+		int cost = 0;
+		for (int i = solution.size() - 1; i >= 0; i--) {
 			Node node = solution.get(i);
 
-			if(i==solution.size()-1) {
+			if (i == solution.size() - 1) {
 				System.out.println("Initial Field: ");
 				node.getState().printField();
 				System.out.println();
 			} else {
 				cost += State.cost(node.getAction());
 				State st = node.getState();
-				
-				/*System.out.println(node.getAction().getNext_move().getX());
-				System.out.println(node.getAction().getNext_move().getY());
-				int north = st.getX()-1, south = st.getX()+1, east = st.getY()+1, west = st.getY()-1;
-				System.out.println("Action: [(" + solution.get(i).getAction().getSand_n() + ", (" + st.getX() + "," + st.getY() + ")) ," 
-						+ "(" + solution.get(i).getAction().getSand_s() + ", (" + st.getX()+ "," + st.getY() + "))"
-						+ "(" + solution.get(i).getAction().getSand_w() + ", (" + st.getX() + "," + west + "))"
-						+ "(" + solution.get(i).getAction().getSand_e() + ", (" + st.getX() + "," + east + "))]");*/
-				
-				System.out.println(node.getAction().toString() + node.getAction().getNext_move().printMove() + " Cost of Action: " + State.cost(node.getAction()) + " Total cost " + cost);
-				
-				int [][] field = st.getField();
-				
-				for(int j=0; j<field.length; j++) {
-					for(int x=0; x<field.length; x++) {
+
+				/*
+				 * System.out.println(node.getAction().getNext_move().getX());
+				 * System.out.println(node.getAction().getNext_move().getY()); int north =
+				 * st.getX()-1, south = st.getX()+1, east = st.getY()+1, west = st.getY()-1;
+				 * System.out.println("Action: [(" + solution.get(i).getAction().getSand_n() +
+				 * ", (" + st.getX() + "," + st.getY() + ")) ," + "(" +
+				 * solution.get(i).getAction().getSand_s() + ", (" + st.getX()+ "," + st.getY()
+				 * + "))" + "(" + solution.get(i).getAction().getSand_w() + ", (" + st.getX() +
+				 * "," + west + "))" + "(" + solution.get(i).getAction().getSand_e() + ", (" +
+				 * st.getX() + "," + east + "))]");
+				 */
+
+				System.out.println(node.getAction().toString() + " Next move: " + node.getAction().getNext_move().printMove()
+						+ " Cost of Action: " + State.cost(node.getAction()) + " Total cost " + cost);
+
+				int[][] field = st.getField();
+
+				for (int j = 0; j < field.length; j++) {
+					for (int x = 0; x < field.length; x++) {
 						System.out.print(field[j][x]);
 					}
 					System.out.println();
@@ -296,19 +284,16 @@ public class Problem {
 				System.out.println();
 			}
 		}
-		
-		System.out.println("The total cost is " + cost + " and the total depth is " + solution.get(0).getDepth());
+		double time = time_complexity / 1000.00;
+		System.out.println("The total cost is " + cost + " and the total depth is " + solution.get(0).getDepth() 
+				+ "\nSpatial complexity: " + spatial_complexity + ". Time complexity: " + time + " seconds.");
 	}
-	
-	
-	
-	
-	
-	public static String encriptaEnMD5(State st) {
+
+	public static String encryptState(State st) {
 		try {
 			int message = st.getK() + st.getMax() + st.getN_cols() + st.getN_rows() + st.getX() + st.getY();
-			for (int i=0; i<st.getField().length; i++) {
-				for(int j=0; j<st.getField().length; j++) {
+			for (int i = 0; i < st.getField().length; i++) {
+				for (int j = 0; j < st.getField().length; j++) {
 					message += st.getField()[i][j];
 				}
 			}
@@ -326,12 +311,8 @@ public class Problem {
 			return null;
 		}
 	}
-	
+
 	public static final byte[] intToByteArray(int value) {
-	    return new byte[] {
-	            (byte)(value >>> 24),
-	            (byte)(value >>> 16),
-	            (byte)(value >>> 8),
-	            (byte)value};
+		return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16), (byte) (value >>> 8), (byte) value };
 	}
 }
--- src/Domain/State.java
+++ src/Domain/State.java
@@ -1,4 +1,5 @@
 package Domain;
+
 import java.util.*;
 
 public class State {
@@ -7,11 +8,10 @@ public class State {
 	private int K;
 	private int max;
 	private int X;
-	private int Y; 
-	private int [][] field;
+	private int Y;
+	private int[][] field;
 	private int value;
 
-	
 	public State() {
 		this.n_rows = 0;
 		this.n_cols = 0;
@@ -20,7 +20,7 @@ public class State {
 		X = 0;
 		Y = 0;
 	}
-	
+
 	public int getValue() {
 		return value;
 	}
@@ -29,30 +29,34 @@ public class State {
 		this.value = value;
 	}
 
-	
 	public int getN_rows() {
 		return n_rows;
 	}
+
 	public void setN_rows(int n_rows) {
 		this.n_rows = n_rows;
 	}
-	
+
 	public int getN_cols() {
 		return n_cols;
 	}
+
 	public void setN_cols(int n_cols) {
 		this.n_cols = n_cols;
 	}
-	
+
 	public int getK() {
 		return K;
 	}
+
 	public void setK(int k) {
 		K = k;
 	}
+
 	public int getMax() {
 		return max;
 	}
+
 	public void setMax(int max) {
 		this.max = max;
 	}
@@ -60,63 +64,64 @@ public class State {
 	public int getX() {
 		return X;
 	}
+
 	public void setX(int x) {
 		X = x;
 	}
+
 	public int getY() {
 		return Y;
 	}
+
 	public void setY(int y) {
 		Y = y;
 	}
+
 	public int[][] getField() {
-		int [][] aux = new int [n_cols][n_rows];
-		
-		for (int i=0; i<field.length; i++) {
+		int[][] aux = new int[n_cols][n_rows];
+
+		for (int i = 0; i < field.length; i++) {
 			System.arraycopy(field[i], 0, aux[i], 0, field[i].length);
 		}
-		
+
 		return aux;
 	}
+
 	public void setField(int[][] field) {
 		this.field = field;
 	}
-	
-	
-	
-	
-	public static List<Movement> moveTractor (State t) {
-		List <Movement> pos_moves = new ArrayList <Movement>();
-		
+
+	public static List<Movement> moveTractor(State t) {
+		List<Movement> pos_moves = new ArrayList<Movement>();
+
 		if (0 <= t.getX() && t.getX() < t.getN_rows()) {
-			if(t.getX() + 1 < t.getN_rows()) {
-				Movement s = new Movement (t.getX() +1, t.getY());
+			if (t.getX() + 1 < t.getN_rows()) {
+				Movement s = new Movement(t.getX() + 1, t.getY());
 				pos_moves.add(s);
 			}
-			
-			if(t.getX() - 1 >= 0) {
-				Movement n = new Movement(t.getX() -1, t.getY());
+
+			if (t.getX() - 1 >= 0) {
+				Movement n = new Movement(t.getX() - 1, t.getY());
 				pos_moves.add(n);
 			}
 		}
-		
+
 		if (0 <= t.getY() && t.getY() < t.getN_cols()) {
-			if(t.getY() + 1 < t.getN_cols()) {
-				Movement e = new Movement(t.getX(), t.getY()+1);
+			if (t.getY() + 1 < t.getN_cols()) {
+				Movement e = new Movement(t.getX(), t.getY() + 1);
 				pos_moves.add(e);
 			}
-			
-			if(t.getY() - 1 >= 0) {
-				Movement w = new Movement (t.getX(), t.getY()-1);
+
+			if (t.getY() - 1 >= 0) {
+				Movement w = new Movement(t.getX(), t.getY() - 1);
 				pos_moves.add(w);
 			}
-		}		
+		}
 		return pos_moves;
-	}	
+	}
 	
 	
-	
-	public static List<Action> successor(State t) {
+	/*public static List<Action> successor(State t) {
 		List<Movement> moves = State.moveTractor(t);
 		List<Action> actions = new ArrayList<Action>();
 		List<int[]> combinations = generate_combinations(t.getMax());
@@ -154,74 +159,115 @@ public class State {
 			}
 		}
 		return actions;
-	}
+	}*/
+
+	public static List<Action> successor(State t) {
+		List<Movement> moves = State.moveTractor(t);
+		List<Action> actions = new ArrayList<Action>();
+		int [][] field = t.getField();
+		int sand = field[t.getX()][t.getY()] - t.getK();
+		if(sand<0) {
+			sand=0;
+		}
+		List<int[]> combinations = generate_combinations(sand);
 		
+		int [] aux = new int[4];
+		
+		for (int i=0; i<moves.size(); i++) {
+			for (int j=0; j<combinations.size(); j++) {
+				int sum_sand = 0;
+				
+				for (int s=0; s<4; s++) {
+					aux = combinations.get(j);
+					sum_sand = sum_sand + aux[s];
+				}
+		
+				/*if (field[t.getX()][t.getY()] - t.getK() > 0) {
+					sand = field[t.getX()][t.getY()] - t.getK();*/
+			
+					if(sum_sand==sand) { 
+						if (move_Sand(t,aux)) {
+							Movement mv = new Movement (moves.get(i).getX(), moves.get(i).getY());
+							Action ac = new Action (mv, aux[0], aux[1], aux[2], aux[3]);
+							actions.add(ac);
+						}
+					}
+				//}
+			}
+			
+			/*if (field[t.getX()][t.getY()] - t.getK() < 0) {
+				Movement mv = new Movement (moves.get(i).getX(), moves.get(i).getY());
+				Action ac = new Action (mv, 0, 0, 0, 0);
+				actions.add(ac);
+			}*/
+		}
+		return actions;
+	}
+
 	@Override
 	public String toString() {
-		return "State [n_rows=" + n_rows + ", n_cols=" + n_cols + ", K=" + K + ", max=" + max 
-				+ ", X=" + X + ", Y=" + Y + ", field=" + Arrays.toString(field) + "]";
+		return "State [n_rows=" + n_rows + ", n_cols=" + n_cols + ", K=" + K + ", max=" + max + ", X=" + X + ", Y=" + Y
+				+ ", field=" + Arrays.toString(field) + "]";
 	}
 
-
-
 	public static List<int[]> generate_combinations(int max) {
-		List <int[]> combinations = new ArrayList<int[]> ();
-		
-	    for(int i=0; i<=max; i++) {
-			for (int j=0; j<=max; j++) {
-				for (int s=0; s<=max; s++) {
-					for (int c=0; c<=max;c++) {
-						int [] comb = {i,j,s,c};
+		List<int[]> combinations = new ArrayList<int[]>();
+
+		for (int i = 0; i <= max; i++) {
+			for (int j = 0; j <= max; j++) {
+				for (int s = 0; s <= max; s++) {
+					for (int c = 0; c <= max; c++) {
+						int[] comb = { i, j, s, c };
 						combinations.add(comb);
 					}
 				}
 			}
-	    }   
-	    return combinations;
+		}
+		return combinations;
 	}
-	
-	public static boolean move_Sand (State t, int [] sand) {
-		int [][] field = t.getField();
+
+	public static boolean move_Sand(State t, int[] sand) {
+		int[][] field = t.getField();
 		boolean value = true;
-		
-		if(sand[0] > 0) {
-			if (t.getX()-1 >= 0 && field[t.getX() - 1] [t.getY()] + sand [0] <= t.getMax()) {
+
+		if (sand[0] > 0) {
+			if (t.getX() - 1 >= 0 && field[t.getX() - 1][t.getY()] + sand[0] <= t.getMax()) {
 				value = true;
 			} else {
 				return false;
 			}
 		}
-		
-		if(sand[1] > 0) {
-			if (t.getX()+1 < field.length && field[t.getX() + 1] [t.getY()] + sand [1] <= t.getMax()) {
+
+		if (sand[1] > 0) {
+			if (t.getX() + 1 < field.length && field[t.getX() + 1][t.getY()] + sand[1] <= t.getMax()) {
 				value = true;
 			} else {
 				return false;
 			}
 		}
-		
-		if(sand[2] > 0) {
-			if (t.getY()+1 < field.length && field[t.getX()] [t.getY() + 1] + sand [2] <= t.getMax()) {
+
+		if (sand[2] > 0) {
+			if (t.getY() + 1 < field.length && field[t.getX()][t.getY() + 1] + sand[2] <= t.getMax()) {
 				value = true;
 			} else {
 				return false;
 			}
 		}
-		
-		if(sand[3] > 0) {
-			if (t.getY()-1 >= 0 && field[t.getX()] [t.getY() - 1] + sand [3] <= t.getMax()) {
+
+		if (sand[3] > 0) {
+			if (t.getY() - 1 >= 0 && field[t.getX()][t.getY() - 1] + sand[3] <= t.getMax()) {
 				value = true;
 			} else {
 				return false;
 			}
 		}
-		
+
 		return value;
 	}
-	
-	public static State copyState(State st, Action ac, int [][] new_field) {
+
+	public static State copyState(State st, Action ac, int[][] new_field) {
 		State newState = new State();
-		
+
 		newState.setField(new_field);
 		newState.setX(ac.getNext_move().getX());
 		newState.setY(ac.getNext_move().getY());
@@ -229,27 +275,23 @@ public class State {
 		newState.setMax(st.getMax());
 		newState.setN_cols(st.getN_cols());
 		newState.setN_rows(st.getN_rows());
-		
+
 		return newState;
 	}
-	
-	
-	
-	public static int cost (Action ac) {
-		int moved_sand = ac.getSand_e()+ ac.getSand_n() + ac.getSand_s() + ac.getSand_w();
-		int cost =  moved_sand + 1;
+
+	public static int cost(Action ac) {
+		int moved_sand = ac.getSand_e() + ac.getSand_n() + ac.getSand_s() + ac.getSand_w();
+		int cost = moved_sand + 1;
 		return cost;
 	}
-	
+
 	public void printField() {
-		for (int i=0; i<n_rows; i++) {
-			for (int j=0; j<n_cols; j++) {
+		for (int i = 0; i < n_rows; i++) {
+			for (int j = 0; j < n_cols; j++) {
 				System.out.print(field[i][j]);
 			}
 			System.out.println();
 		}
 	}
-	
-	
 
 }
