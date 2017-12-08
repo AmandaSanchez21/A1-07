6371f9332cc1880c38e4e08d96f4ab351616f0b9
AmandaSanchez21 <32260263+AmandaSanchez21@users.noreply.github.com>
2017-12-08 21:15:19 +0100

    Add files via upload

---
+++ Movement.java
@@ -0,0 +1,31 @@
+package Domain;
+
+public class Movement {
+	private int X, Y;
+	
+	public Movement(int x, int y) { 
+		this.X = x;
+		this.Y = y;
+	}
+	
+	public int getX() {
+		return X;
+	}
+
+	public void setX(int x) {
+		X = x;
+	}
+
+	public int getY() {
+		return Y;
+	}
+
+	public void setY(int y) {
+		Y = y;
+	}	
+	
+	
+	public String printMove() {
+		return "[" + X + "," + Y + "]";
+	}
+}
